d9c1d021a99b953b9fe7436ed5f016327ce0fcce
Camilly Dantas <camillysdantas@gmail.com>
2021-01-10 14:25:55 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/camilly/acamideliver/AcamideliverApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.camilly.acamideliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcamideliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcamideliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/camilly/acamideliver/AcamideliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.camilly.acamideliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcamideliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
